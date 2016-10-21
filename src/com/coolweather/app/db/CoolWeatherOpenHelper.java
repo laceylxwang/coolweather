a76f4cc5cdd16c0b3c4978b0d58d77fe96837277
wanglixin <wanglixin@yulong.com>
2016-10-21 15:29:14 +0800

    Fourth commit

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -5,9 +5,23 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
-	
+	public static final String CREATE_PROVINCE = "create table Province(" +
+			"id integer primary key autoincrement, " +
+			"province_name text, " +
+			"province_code text)";
+	public static final String CREATE_CITY = "create table City(" +
+			"id integer primary key autoincrement, " +
+			"city_name text, " +
+			"city_code text, " +
+			"province_id integer)";
+	public static final String CREATE_COUNTY = "create table County(" +
+			"id integer primary key autoincrement, " +
+			"county_name text, " +
+			"county_code text, " +
+			"city_id integer)"; 
 	
 	public CoolWeatherOpenHelper(Context context, String name, CursorFactory 
 			factory, int version){
@@ -15,23 +29,12 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
 	}
 	
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("create table Province(" +
-				"id integer primary key autoincrement," +
-				"province_name text, " +
-				"province_code text" +
-				");");
-		db.execSQL("create teble City(" +
-				"id integer primary key autoincrement," +
-				"city_name text" +
-				"city_code text" +
-				"province_id integer" +
-				");");
-		db.execSQL("create table Country(" +
-				"id integer primary key autoincrement," +
-				"country_name text," +
-				"country_code text," +
-				"city_id integer" +
-				");");
+		
+		db.execSQL(CREATE_PROVINCE);
+		
+		db.execSQL(CREATE_CITY);
+		
+		db.execSQL(CREATE_COUNTY);
 		
 	}
 	
